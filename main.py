513f1710089880e839186daf82cb6d3820238963
DaviDTR1 <davidluffy01@gmail.com>
2024-04-11 13:42:16 -0400

    Pipeline

--- main.py
+++ main.py
@@ -9,6 +9,23 @@ from sklearn.preprocessing import OneHotEncoder
 
 X_full = pd.read_csv('data/melb_data.csv')
 
+#remove missing target
+X_full.dropna(axis=0, subset=['Price'], inplace=True)
+y = X_full.Price
+X_full.drop(['Price'], axis=1, inplace=True)
+
+#split train data
+X_train, X_valid, y_train, y_valid = train_test_split(X_full, y, train_size=0.8, test_size=0.2 , random_state=0)
+
+categorical_cols = [cname for cname in X_train.columns if X_train[cname].nunique() < 10 and 
+                        X_train[cname].dtype == "object"]
+
+numerical_cols = [cname for cname in X_train.columns if X_train[cname].dtype in ['int64', 'float64']]
+
+cols = categorical_cols + numerical_cols
+X_train = X_train[cols]
+X_valid = X_valid[cols]
+
 #preprocessing numerical data
 numerical_transformer = SimpleImputer(strategy='constant')
 
@@ -27,4 +44,15 @@ preproccesor = ColumnTransformer(
 
 model = RandomForestRegressor(n_estimators=100, random_state=0)
 
+my_pipeline = Pipeline(steps=[('preprocessor', preproccesor),
+                              ('model', model)
+                              ])
+
+# preprocessing of training data 
+my_pipeline.fit(X_train,y_train)
+
+pred = my_pipeline.predict(X_valid)
+mae = mean_absolute_error(pred, y_valid)
+
+print(mae) 
 
